f08ff3962e36e22ddca8569e0572f4ad445d240b
hoony0321 <eogns0321@gmail.com>
2022-12-08 18:27:33 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/company/semocheck/SemocheckApplication.java
@@ -0,0 +1,13 @@
+package com.company.semocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemocheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemocheckApplication.class, args);
+	}
+
+}
