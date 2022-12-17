ea00fc57bc19d4d02ead6a9d49fb4dd4c6d61fb3
hoony0321 <eogns0321@gmail.com>
2022-12-17 23:46:33 +0900

    feat : Not Found Handler 추가

---
+++ src/main/java/com/company/semocheck/config/ServletConfig.java
@@ -0,0 +1,19 @@
+package com.company.semocheck.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class ServletConfig extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(ServletConfig.class, args);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
+}
+
