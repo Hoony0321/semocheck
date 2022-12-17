f3a81ddf4372a291de3366905881ca30312b92bb
hoony0321 <eogns0321@gmail.com>
2022-12-17 23:46:15 +0900

    test : Test Controller 추가

--- src/main/java/com/company/semocheck/controller/TestController.java
+++ src/main/java/com/company/semocheck/controller/TestController.java
@@ -1,21 +1,32 @@
 package com.company.semocheck.controller;
 
+import com.company.semocheck.common.response.DataResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Tag(name = "테스트", description = "테스트용 API 모음입니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/test")
+@RequestMapping("/api/test")
 public class TestController {
 
-    @Value("${spring.oauth2.google.client-id}")
-    private String test;
-
+    @Operation(summary = "test api 호출", description = "test api 호출 메서드입니다.")
     @GetMapping("/")
-    public String test(){
-        return test;
+    public DataResponseDto<List> test(HttpServletRequest request){
+
+        List testData = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        return DataResponseDto.of(testData, "테스트 API입니다.");
     }
+
+
 }
