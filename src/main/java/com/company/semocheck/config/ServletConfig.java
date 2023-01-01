cbba7b59b505755ae0f8c727db8fd47fc45daa54
hoony0321 <eogns0321@gmail.com>
2023-01-01 12:16:07 +0900

    feat : path match 특수 문자 가능하게 추가

--- src/main/java/com/company/semocheck/config/ServletConfig.java
+++ src/main/java/com/company/semocheck/config/ServletConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@ComponentScan
+@ComponentScan()
 @EnableAutoConfiguration
 public class ServletConfig extends SpringBootServletInitializer {
     public static void main(String[] args) {
---
+++ src/main/java/com/company/semocheck/config/TomcatWebConfig.java
@@ -0,0 +1,15 @@
+package com.company.semocheck.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatWebConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+    }
+}
