4c80067372b9afe98e954a23c61393fbf6ee4a5a
hoony0321 <eogns0321@gmail.com>
2023-01-01 12:21:45 +0900

    refactor : Member domain / repository / service 수정
    oauth token login 방식 진행을 위해 필요한 메서드 추가

--- src/main/java/com/company/semocheck/domain/Member.java
+++ src/main/java/com/company/semocheck/domain/Member.java
@@ -3,13 +3,17 @@ package com.company.semocheck.domain;
 import com.company.semocheck.auth.oauth2.OAuth2Attributes;
 import com.company.semocheck.domain.dto.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 public class Member extends BaseTimeEntity{
 
     @Id
@@ -17,35 +21,60 @@ public class Member extends BaseTimeEntity{
     @Column(name = "member_id")
     private Long id;
 
+    @Column(name = "oauth_id", nullable = false)
+    private String oAuthId;
+
     @Column(nullable = false)
-    private String name;
+    private String provider;
 
     @Column(nullable = false)
     private String email;
 
-    @Column
+    @Size(max = 15)
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
     private String picture;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
 
+    @Column(nullable = false)
+    private Boolean agree_notify;
+
+    private Boolean sex;
+
+    private Integer age;
+
     @Builder
-    public Member(String name, String email, String picture, Role role) {
-        this.name = name;
+    public Member(String oAuthId, String provider, String email, String name, String picture, Role role, Boolean agree_notify, Boolean sex, Integer age) {
+        this.oAuthId = oAuthId;
+        this.provider = provider;
         this.email = email;
+        this.name = name;
         this.picture = picture;
         this.role = role;
-    }
-
-    public Member update(String name, String picture){
-        this.name = name;
-        this.picture = picture;
-
-        return this;
+        this.agree_notify = agree_notify;
+        this.sex = sex;
+        this.age = age;
     }
 
     public String getRoleKey(){
         return this.role.getKey();
+    }
+
+    static public Member createNewMember(OAuth2Attributes attributes, String provider){
+        Member member = new Member();
+        member.oAuthId = attributes.getId();
+        member.provider = provider;
+        member.email = attributes.getEmail();
+        member.picture = attributes.getPicture();
+        member.name = attributes.getName();
+        member.role = Role.USER;
+        member.agree_notify = false;
+
+        return member;
     }
 }
--- src/main/java/com/company/semocheck/service/MemberService.java
+++ src/main/java/com/company/semocheck/service/MemberService.java
@@ -1,11 +1,11 @@
 package com.company.semocheck.service;
 
+import com.company.semocheck.auth.oauth2.OAuth2Attributes;
 import com.company.semocheck.common.response.Code;
 import com.company.semocheck.domain.Member;
 import com.company.semocheck.exception.GeneralException;
 import com.company.semocheck.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +19,30 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public Long joinMember(OAuth2Attributes attributes, String provider){
+        Member member = Member.createNewMember(attributes, provider);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
     public List<Member> findAll(){ return memberRepository.findAll(); }
+    public Member findById(Long id){
+        Optional<Member> findOne = memberRepository.findById(id);
+        if(findOne.isEmpty()) throw new GeneralException(Code.NOT_FOUND, "해당 정보의 회원은 존재하지 않습니다.");
+
+        return findOne.get();
+    }
 
     public Member findByEmail(String email){
         Optional<Member> findOne = memberRepository.findByEmail(email);
-        if(findOne.isEmpty()) throw new GeneralException(Code.NOT_FOUND);
+        if(findOne.isEmpty()) throw new GeneralException(Code.NOT_FOUND, "해당 정보의 회원은 존재하지 않습니다.");
 
         return findOne.get();
+    }
+
+    public Optional<Member>  findByOAuthIdAndProvider(String oAuthId, String provider){
+        return memberRepository.findByoAuthIdAndProvider(oAuthId, provider);
+
     }
 }
