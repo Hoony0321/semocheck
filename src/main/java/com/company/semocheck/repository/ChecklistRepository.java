ed25a4bd62ed07878cae8915e619c1b71a29204e
hoony0321 <eogns0321@gmail.com>
2023-02-26 20:56:42 +0900

    feat : checklist add column
    temporary 칼럼 추가 -> 임시저장 판단 여부

--- src/main/java/com/company/semocheck/repository/ChecklistRepository.java
+++ src/main/java/com/company/semocheck/repository/ChecklistRepository.java
@@ -2,13 +2,18 @@ package com.company.semocheck.repository;
 
 import com.company.semocheck.domain.Checklist;
 import com.company.semocheck.domain.Member;
-import com.company.semocheck.domain.SubCategory;
 import org.hibernate.annotations.Check;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
-    List<Checklist> findByPublishTrue();
-    List<Checklist> findByOwner(Member owner);
+
+    Optional<Checklist> findByIdAndTemporaryIsNull(Long id);
+    Optional<Checklist> findByIdAndTemporaryIsNotNull(Long id);
+    List<Checklist> findByTemporaryIsNullAndPublishIsTrue();
+    List<Checklist> findByOwnerAndTemporaryIsNull(Member owner);
+    List<Checklist> findByOwnerAndTemporaryIsNotNull(Member owner);
 }
