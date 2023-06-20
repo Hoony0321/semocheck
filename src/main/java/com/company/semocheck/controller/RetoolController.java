966eef980f13c20feec873cb222e64433bc56038
hoony0321 <eogns0321@gmail.com>
2023-06-20 20:53:36 +0900

    feat : add delete checklist api to RetoolController

--- src/main/java/com/company/semocheck/controller/RetoolController.java
+++ src/main/java/com/company/semocheck/controller/RetoolController.java
@@ -4,17 +4,20 @@ import com.company.semocheck.common.response.ApiDocumentResponse;
 import com.company.semocheck.common.response.Code;
 import com.company.semocheck.common.response.DataResponseDto;
 import com.company.semocheck.common.response.ResponseDto;
+import com.company.semocheck.domain.checklist.Checklist;
+import com.company.semocheck.domain.dto.FileDto;
+import com.company.semocheck.domain.dto.Role;
+import com.company.semocheck.exception.GeneralException;
+import com.company.semocheck.form.CreateChecklistForm;
 import com.company.semocheck.domain.member.Member;
-import com.company.semocheck.domain.request.checklist.CreateChecklistRequest;
+import com.company.semocheck.service.FileService;
 import com.company.semocheck.service.MemberService;
 import com.company.semocheck.service.checklist.ChecklistService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "리툴", description = "리툴 관련 API 모음입니다.")
 @RestController
@@ -25,22 +28,44 @@ public class RetoolController {
     private final MemberService memberService;
     private final ChecklistService checklistService;
 
+    private final CategoryController categoryController;
+
     //TODO : admin path url 포함시키기
     @ApiDocumentResponse
     @Operation(summary = "Create new checklist API by admin", description = "[어드민 전용] 새로운 체크리스트를 생성합니다.\n\n" +
             "필수 목록 : [title]")
     @PostMapping(value = "/checklists")
-    private ResponseDto createChecklist(@RequestBody CreateChecklistRequest requestDto){
-        // get member by jwt token
+    private ResponseDto createChecklist(@RequestBody CreateChecklistForm createForm){
         Member admin = memberService.findById(1l);
 
         // validate requestDto
-        requestDto.validate();
+        createForm.validate();
+
+        // image setting
+        if(createForm.getImageId() == null){
+            DataResponseDto<FileDto> defaultImageFileDto = categoryController.getDefaultImage(createForm.getMainCategoryName(), createForm.getSubCategoryName());
+            createForm.setDefaultImageId(defaultImageFileDto.getData().getId());
+        }
 
         // create a checklist Entity
-        Long checklistId = checklistService.createChecklist(requestDto, admin);
+        Long checklistId = checklistService.createChecklist(createForm, admin);
 
         return DataResponseDto.of(checklistId, Code.SUCCESS_CREATE);
+    }
+
+    @ApiDocumentResponse
+    @Operation(summary = "Delete a checklist API", description = "[어드민 전용] 회원의 체크리스트를 삭제합니다.\n\n")
+    @DeleteMapping("/checklists/{checklist_id}")
+    private ResponseDto deleteChecklist(@PathVariable("checklist_id") Long checklistId){
+        Member admin = memberService.findById(1l);
+
+        //Get checklist by id
+        Checklist checklist = checklistService.findById(checklistId);
+
+        //Delete checklist
+        checklistService.deleteChecklist(checklist, admin);
+
+        return ResponseDto.of(true, Code.SUCCESS_DELETE);
     }
 
 }
